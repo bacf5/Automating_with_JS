e70d0312118e732585dc194b314b3bff438321a1
bacf5 <brunoagustincarusofassa@gmail.com>
2022-11-11 11:48:22 -0300

    Editing text files automatically  with NodeJS

--- editText/index.js
+++ editText/index.js
@@ -1,23 +1,23 @@
-const fs = require("fs");
+const fs = require('fs');
 
-let fileString = fs.readFileSync("input.txt").toString();
+let fileString = fs.readFileSync('input.txt').toString();
 
-//Fill out the initials
-fileString = fileString.replace(/K:/g, "Kevin: ");
-fileString = fileString.replace(/E:/g, "Eric: ");
+// Fill out the initials
+fileString = fileString.replace(/K:/g, 'Bruno: ');
+fileString = fileString.replace(/E:/g, 'Esteban: ');
+fileString = fileString.replace(/kevin/g, 'Bruno ');
 
-//Add line number
-const characterArray = fileString.split("");
+// Add line number
+const characterArray = fileString.split(''); // --> 'hellomax'.split("") = ['h','e','l','l' ... etc]
 let lineNumber = 1;
 
 for (let i = 0; i < characterArray.length; i++) {
   const prevChar = characterArray[i - 1];
-
-  if (prevChar === "\n" || !prevChar) {
-    characterArray[i] = String(lineNumber) + ". " + characterArray[i];
+  if (prevChar === '\n' || !prevChar) {
+    characterArray[i] = String(lineNumber) + '. ' + characterArray[i];
     lineNumber++;
   }
 }
+fileString = characterArray.join('');
 
-fileString = characterArray.join("");
-fs.writeFileSync("output.txt", fileString);
+fs.writeFileSync('output.txt', fileString);
